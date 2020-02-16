953fc1a8e64e4404a9c6821794528ed653db7342
mschenatti <mschenatti@topcon.com>
2020-02-16 22:33:38 +0100

    First inabox implementation

--- arduinoBeerSensorProject/arduinoBeerSensorProject.ino
+++ arduinoBeerSensorProject/arduinoBeerSensorProject.ino
@@ -1,5 +1,3 @@
-#include <RTClib.h>
-
 /*--------------INLCUDES--------------*/
 //Monitor
 #include <LiquidCrystal_I2C.h>
@@ -12,24 +10,29 @@
 #include <Rtc_Pcf8563.h>
 
 /*--------------DEFINES--------------*/
-#define LED_STOP_PIN 4
-#define INPUT_STOP_PIN 5
-#define INPUT_SELECT 9
-#define INPUT_CHANGE 8
+//Relè
+#define RELE1PIN 2
+#define RELE2PIN 3
+#define DEFAULT_RELE1_LOW_TEMP  19
+#define DEFAULT_RELE1_HIGH_TEMP 21
+#define DEFAULT_RELE2_LOW_TEMP  19
+#define DEFAULT_RELE2_HIGH_TEMP 21
 
-//Monitor
 //Memory
-#define LED_FS_W_PIN 3
 #define SS_PIN 10
+
 //Sensor
-#define DHTPIN 7     // what pin we're connected to
+#define DHT1PIN 6     // what pin we're connected to
+#define DHT2PIN 7     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
 /*--------------VARIABLES--------------*/
+//Relè
+bool Rele1On = false;
+bool Rele2On = false;
+
 //Monitor
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-String raw1 = "gg/mm/aaaa hh:mm";
-String raw2 = "T:--C H:--%";
 
 //Memory
 byte inByte;
@@ -37,10 +40,12 @@ bool sdInitSuccess = false; //card init status
 File myFile;
 int lineCounter = 0;
 bool stopRequired = false;
+int memoryWritingPeriod = 10;
+int minuteWriting = -1;
 
 //Sensor
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-int chk;
+DHT dht1(DHT1PIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht2(DHT2PIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
 
@@ -48,19 +53,7 @@ float temp; //Stores temperature value
 Rtc_Pcf8563 rtc;
 String rtcTime;
 String rtcDate;
-typedef struct dateVar{
-  int day;
-  int month;
-  int year;
-  int hours;
-  int minutes;
-}sDateVar;
-sDateVar date;
-int minuteWriting = -1;
 
-
-//TERMISTOR
-bool termistorStatus = false;
 /*--------------SETUP--------------*/
 void setup() {
   //Debug
@@ -69,90 +62,103 @@ void setup() {
     ; //wait for the serial port to connect.
   }
 
+  /*Serial.println("Bootloading-Press 'c' to set RTC");
+  int count = 5;
+  while(count>0){
+    Serial.println(count);
+    count--;
+    
+    if (Serial.available() > 0) {
+      int incomingByte = 0;
+      // read the incoming byte:
+      incomingByte = Serial.read();
+    
+      // say what you got:
+      Serial.print("I received: ");
+      Serial.println(incomingByte);
+  
+      if(incomingByte == 'c'){
+        Serial.println("***CLOCK SETUP***");
+        byte day = 0;
+        byte month = 0;
+        byte year = 0;
+        byte hour = 0;
+        byte minute = 0;
+        while(Serial.available()){Serial.read();}
+        Serial.print("Day (1:31) ");    while(!Serial.available()){day = Serial.read();}//day = Serial.read();
+        Serial.print("Month (1:12) ");  while(!Serial.available()){month = Serial.read();}
+        Serial.print("Year (0:99) ");   while(!Serial.available()){year = Serial.read();}
+        Serial.print("Hour (0:23) ");   while(!Serial.available()){hour = Serial.read();}
+        Serial.print("Minute (0:59) "); while(!Serial.available()){minute = Serial.read();}
+        rtc.initClock();
+        rtc.setDate(day,1,month,0, year); 
+        rtc.setTime(hour, minute, 0);
+        //rtc.initClock();
+      }
+  }
+  
+    delay(1000);
+  }*/
+
+  //RTC
+  //rtc.initClock();
+  //rtc.setDate(16,1,2,0,20); 
+  //rtc.setTime(22,24,1);
+        
   //Monitor
   lcd.init();
   lcd.backlight();
-
+  
   //Memory
   pinMode(SS_PIN, OUTPUT);
-  pinMode(LED_FS_W_PIN, OUTPUT);
-  digitalWrite(LED_FS_W_PIN, LOW);
   delay(1000);
   sdInitSuccess = initSD();
 
-  //Input/Output
-  pinMode(LED_STOP_PIN, OUTPUT);
-  pinMode(INPUT_STOP_PIN, INPUT);
+  //Relè
+  pinMode(RELE1PIN, OUTPUT);
+  pinMode(RELE2PIN, OUTPUT);
+  digitalWrite(RELE1PIN, HIGH);
+  digitalWrite(RELE2PIN, HIGH);
+  Serial.println("Relè 1 OFF");
+  Serial.println("Relè 2 OFF");
+  Rele1On = false;
+  Rele2On = false;
 
   //Sensor
-  dht.begin();
+  dht1.begin();
+  dht2.begin();
 
-  //RTC
-  if(sdInitSuccess){
-    if(SD.exists("DATE.txt")){
-      Serial.println("Date setting");
-      File dateFile;
-      char buf[15];
-      String sBuf;
-      dateFile = SD.open("DATE.txt", FILE_READ);
-      if(dateFile){
-        date.day = dateFile.parseInt();
-        date.month = dateFile.parseInt();
-        date.year = dateFile.parseInt();
-        date.hours = dateFile.parseInt();
-        date.minutes = dateFile.parseInt();
-        dateFile.close();
-        rtc.initClock();
-        rtc.setDate(date.day, 1, date.month, 0, date.year); 
-        rtc.setTime(date.hours, date.minutes, 0);
-      }
-      Serial.println("Date file removing");
-      SD.remove("DATE.txt");
-    }
-  }
-
-  //TERMISTOR
-  digitalWrite(LED_STOP_PIN, LOW);
-  Serial.println("Termoresistor OFF");
-  termistorStatus = false;
 }
 
 /*--------------LOOP--------------*/
 void loop() {
-//  stopRequired = digitalRead(INPUT_STOP_PIN);
-//  if(stopRequired){
-//    Serial.println("STOP REQUIRED");
-//    while(stopRequired){
-//      stopRequired = digitalRead(INPUT_STOP_PIN);
-//      delay(5000);
-//    }
-//    Serial.println("START REQUIRED");
-//  }else{
-//  }
-  
-  //Getting sensor value
-  hum = dht.readHumidity();
-  temp= dht.readTemperature();
+  String raw1 = "gg/mm/aaaa hh:mm";
+  String raw2 = "T:--C H:--%";
 
-  if(temp < 18 && termistorStatus == false){
-    digitalWrite(LED_STOP_PIN, HIGH);
-    Serial.println("Termoresistor ON");
-    termistorStatus = true;
-  }
-  else if(temp > 19 && termistorStatus == true)
-  {
-    digitalWrite(LED_STOP_PIN, LOW);
-    Serial.println("Termoresistor OFF");
-    termistorStatus = false;
-  }
-  else
-  {
-    //Do nothing
-  }
+  int minutesNow = 0;
+  
   //Getting Time & Date
   rtcTime = rtc.formatTime(RTCC_TIME_HM);
   rtcDate = rtc.formatDate(RTCC_DATE_WORLD);
   
+  //Getting sensor value
+  hum = dht1.readHumidity();
+  temp= dht1.readTemperature();
+  //Debug write
+  Serial.println("DHT22 1");
+  printDebugTempHum();
+  manageRele(1, temp, DEFAULT_RELE1_LOW_TEMP, DEFAULT_RELE1_HIGH_TEMP);
+
+  /*
+  //Getting sensor value
+  hum = dht2.readHumidity();
+  temp= dht2.readTemperature();
+  //Debug write
+  Serial.println("DHT22 2");
+  printDebugTempHum();
+  manageRele(2, temp, DEFAULT_RELE2_LOW_TEMP, DEFAULT_RELE2_HIGH_TEMP);
+  */
+    
   //Monitor write
   lcd.clear();
   lcd.setCursor(0,0);
@@ -162,21 +168,10 @@ void loop() {
   raw2 = "T:" + getStringFromFloat(temp) + "C H:" + getStringFromFloat(hum) + "%";
   typewriting(raw2);
 
-  //Debug write
-  Serial.print("Humidity: ");
-  Serial.print(hum);
-  Serial.print(" %, Temp: ");
-  Serial.print(temp);
-  Serial.println(" Celsius");
-  Serial.print(rtcTime);
-  Serial.print("\r\n");
-  Serial.print(rtcDate);
-  Serial.print("\r\n");
-
-  date.minutes=rtc.getMinute();
-  if(minuteWriting == -1 || date.minutes == minuteWriting){
+  minutesNow = rtc.getMinute();
+  if(minuteWriting == -1 || minutesNow == minuteWriting){
     saveDataOnFile();
-    minuteWriting = (date.minutes + 10) % 60;
+    minuteWriting = (minutesNow + memoryWritingPeriod) % 60;
   }
   delay(5000);
 }
@@ -196,14 +191,14 @@ bool initSD(void){
     return true;
   }
 }
+
 void saveDataOnFile(void){
-  digitalWrite(LED_FS_W_PIN, HIGH);
   if (sdInitSuccess) {
     myFile = SD.open("TEST.txt", FILE_WRITE);
     if (myFile) {
       Serial.println("File opened successfully.");
       Serial.println("Writing to TEST.text");
-      myFile.print(raw1 + ";" + temp + ";" + hum + ";" + termistorStatus); myFile.println();
+      //myFile.print(raw1 + ";" + temp + ";" + hum + ";" + Rele1On); myFile.println();
       myFile.close(); //this writes to the card
       Serial.println("Done");
       Serial.println();
@@ -215,7 +210,6 @@ void saveDataOnFile(void){
   else if (!sdInitSuccess) { //if not already initialized
       sdInitSuccess = initSD();
   }
-  digitalWrite(LED_FS_W_PIN, LOW);
 }
 
 void typewriting(String messaggio) {
@@ -227,4 +221,60 @@ void typewriting(String messaggio) {
 
 String getStringFromFloat(float var){
   return String(var).substring(0, String(var).length() - 1);
+}
+
+void printDebugTempHum(void) {
+  Serial.print(rtcDate);
+  Serial.print(" ");
+  Serial.print(rtcTime);
+  Serial.print("\r\n");
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+  Serial.println("---");
+}
+
+void manageRele(int rele, int temp, int lowTemp, int highTemp){
+  switch(rele){
+    case 1:
+        if(temp < lowTemp && Rele1On == false){
+          digitalWrite(RELE1PIN, LOW);
+          Serial.println("Relè 1 ON");
+          Rele1On = true;
+        }
+        else if(temp > highTemp && Rele1On == true)
+        {
+          digitalWrite(RELE1PIN, HIGH);
+          Serial.println("Relè 1 OFF");
+          Rele1On = false;
+        }
+        else
+        {
+          //Do nothing
+        }
+      break;
+    case2:
+        if(temp < lowTemp && Rele2On == false){
+          digitalWrite(RELE2PIN, LOW);
+          Serial.println("Relè 2 ON");
+          Rele1On = true;
+        }
+        else if(temp > highTemp && Rele2On == true)
+        {
+          digitalWrite(RELE2PIN, HIGH);
+          Serial.println("Relè 2 OFF");
+          Rele1On = false;
+        }
+        else
+        {
+          //Do nothing
+        }
+      break;
+    default:
+      Serial.println("ERROR - Rele not available");
+      break;
+  }
+  
 }
